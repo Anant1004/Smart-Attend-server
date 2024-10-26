778efeef7898a80917a06f03d1c5a4e108417e64
Anant kumar <anant1042005@gmail.com>
2024-10-26 12:01:04 +0530

    added some changes to urlparser

--- connections/mongoose.js
+++ connections/mongoose.js
@@ -2,12 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI,
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true
-            }
-        );
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to MongoDB successfully.");
     } catch (error) {
         console.error("Failed to connect", error);
