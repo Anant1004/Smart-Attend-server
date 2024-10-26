09842e470d88f1555de62c7a0fffa6b4201c457e
Anant kumar <anant1042005@gmail.com>
2024-10-26 14:14:37 +0530

    randm

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(morgan('dev'));  
 app.use(cors({
-    origin: 'https://smart-attend-puce.vercel.app/',
+    origin: 'https://smart-attend-puce.vercel.app',
     credentials: true,
 }));
 
