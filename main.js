a04810aef9c0942eee2d45f6a7252a7635e23745
Anant kumar <anant1042005@gmail.com>
2024-10-26 12:57:18 +0530

    added some changes to CORS

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(morgan('dev'));  
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://smart-attend-puce.vercel.app/',
     credentials: true,
 }));
 
